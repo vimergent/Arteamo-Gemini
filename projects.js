b35aa399ef26830e0c389053e9e78cb6124613f6
V Issa <vissa@192.168.68.101>
2025-06-28 02:25:02 -0400

    Rename Cyrillic folders and update projects.js paths

--- projects.js
+++ projects.js
@@ -40,13 +40,13 @@ const projects = [
     ]
   },
   {
-    title: "Apartament Траката, Варна_2021",
+    title: "Apartament Trakata, Varna_2021",
     images: [
-      "../Apartament Траката, Варна_2021/1-1-2000x1200.jpg",
-      "../Apartament Траката, Варна_2021/10-1-768x512.jpg",
-      "../Apartament Траката, Варна_2021/11-1-768x576.jpg",
-      "../Apartament Траката, Варна_2021/12-1-768x512.jpg",
-      "../Apartament Траката, Варна_2021/13-768x1132.jpg"
+      "../Apartament Trakata, Varna_2021/1-1-2000x1200.jpg",
+      "../Apartament Trakata, Varna_2021/10-1-768x512.jpg",
+      "../Apartament Trakata, Varna_2021/11-1-768x576.jpg",
+      "../Apartament Trakata, Varna_2021/12-1-768x512.jpg",
+      "../Apartament Trakata, Varna_2021/13-768x1132.jpg"
     ]
   },
   {
