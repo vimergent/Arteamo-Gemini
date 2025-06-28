28c248309d300ab73e5f469e5e06180ec24ad388
V Issa <vissa@192.168.68.101>
2025-06-28 03:07:57 -0400

    Regenerate projects.js with correct URL-encoded Latin folder and image names

--- projects.js
+++ projects.js
@@ -2,91 +2,184 @@ const projects = [
   {
     title: "Apartament Flavia Garden 2024",
     images: [
-      "../Apartament Flavia Garden 2024/cam01.jpg",
-      "../Apartament Flavia Garden 2024/cam02.jpg",
-      "../Apartament Flavia Garden 2024/cam03.jpg",
-      "../Apartament Flavia Garden 2024/cam04.jpg",
-      "../Apartament Flavia Garden 2024/cam05.jpg"
+      "Apartament%20Flavia%20Garden%202024/cam01.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam010.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam011.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam012.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam013.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam014.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam015.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam016.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam017.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam018.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam019.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam02.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam020.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam021.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam022.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam023.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam024_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam025_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam026_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam028_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam029_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam03.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam030_1.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam04.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam05.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam06.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam07.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam08.jpg",
+      "Apartament%20Flavia%20Garden%202024/cam09.jpg"
     ]
   },
   {
     title: "Apartament K55_2021",
     images: [
-      "../Apartament K55_2021/Vladi (1).jpg",
-      "../Apartament K55_2021/Vladi (2).jpg",
-      "../Apartament K55_2021/Vladi (3).jpg",
-      "../Apartament K55_2021/Vladi (4).jpg",
-      "../Apartament K55_2021/Vladi (5).jpg"
+      "Apartament%20K55_2021/Vladi%20(1).jpg",
+      "Apartament%20K55_2021/Vladi%20(10).jpg",
+      "Apartament%20K55_2021/Vladi%20(2).jpg",
+      "Apartament%20K55_2021/Vladi%20(3).jpg",
+      "Apartament%20K55_2021/Vladi%20(4).jpg",
+      "Apartament%20K55_2021/Vladi%20(5).jpg",
+      "Apartament%20K55_2021/Vladi%20(6).jpg",
+      "Apartament%20K55_2021/Vladi%20(7).jpg",
+      "Apartament%20K55_2021/Vladi%20(8).jpg",
+      "Apartament%20K55_2021/Vladi%20(9).jpg"
     ]
   },
   {
-    title: "Apartament Кв. Чайка, Варна_2017",
+    title: "Apartament Kv. Chaika, Varna_2017",
     images: [
-      "../Apartament Кв. Чайка, Варна_2017/Dnevna01.jpg",
-      "../Apartament Кв. Чайка, Варна_2017/Dnevna02.jpg",
-      "../Apartament Кв. Чайка, Варна_2017/Dnevna04.jpg",
-      "../Apartament Кв. Чайка, Варна_2017/Fotev (2).jpg",
-      "../Apartament Кв. Чайка, Варна_2017/Fotev (3).jpg"
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Dnevna01.jpg",
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Dnevna02.jpg",
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Dnevna04.jpg",
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Fotev%20(2).jpg",
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Fotev%20(3).jpg",
+      "Apartament%20Kv.%20Chaika,%20Varna_2017/Fotev%20(5).jpg"
     ]
   },
   {
-    title: "Apartament Симфония - Бриз, Варна_ 2019",
+    title: "Apartament Simfonia - Briz, Varna_ 2019",
     images: [
-      "../Apartament Симфония - Бриз, Варна_ 2019/Alex (1).jpg",
-      "../Apartament Симфония - Бриз, Варна_ 2019/Alex (2).jpg",
-      "../Apartament Симфония - Бриз, Варна_ 2019/Alex (3).jpg",
-      "../Apartament Симфония - Бриз, Варна_ 2019/Alex (4).jpg",
-      "../Apartament Симфония - Бриз, Варна_ 2019/Alex (5).jpg"
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(1).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(10).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(11).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(12).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(13).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(14).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(15).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(16).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(2).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(3).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(4).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(5).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(6).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(7).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(8).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/Alex%20(9).jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/cam016.jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/cam017.jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/cam018.jpg",
+      "Apartament%20Simfonia%20-%20Briz,%20Varna_%202019/cam020.jpg"
     ]
   },
   {
     title: "Apartament Trakata, Varna_2021",
     images: [
-      "Apartament Trakata, Varna_2021/1-1-2000x1200.jpg",
-      "Apartament Trakata, Varna_2021/10-1-768x512.jpg",
-      "Apartament Trakata, Varna_2021/11-1-768x576.jpg",
-      "Apartament Trakata, Varna_2021/12-1-768x512.jpg",
-      "Apartament Trakata, Varna_2021/13-768x1132.jpg"
+      "Apartament%20Trakata,%20Varna_2021/1-1-2000x1200.jpg",
+      "Apartament%20Trakata,%20Varna_2021/10-1-768x512.jpg",
+      "Apartament%20Trakata,%20Varna_2021/11-1-768x576.jpg",
+      "Apartament%20Trakata,%20Varna_2021/12-1-768x512.jpg",
+      "Apartament%20Trakata,%20Varna_2021/13-768x1132.jpg",
+      "Apartament%20Trakata,%20Varna_2021/14-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/15-1-768x512.jpg",
+      "Apartament%20Trakata,%20Varna_2021/16-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/17-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/18-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/19-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/2-1-768x545.jpg",
+      "Apartament%20Trakata,%20Varna_2021/20-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/21-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/22-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/23-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/24-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/25-768x1024.jpg",
+      "Apartament%20Trakata,%20Varna_2021/3-1-768x473.jpg",
+      "Apartament%20Trakata,%20Varna_2021/4-1-768x666.jpg",
+      "Apartament%20Trakata,%20Varna_2021/5-1-768x512.jpg",
+      "Apartament%20Trakata,%20Varna_2021/6-1-768x581.jpg",
+      "Apartament%20Trakata,%20Varna_2021/7-1-768x512.jpg",
+      "Apartament%20Trakata,%20Varna_2021/8-1-768x689.jpg",
+      "Apartament%20Trakata,%20Varna_2021/9-1-768x576.jpg"
     ]
   },
   {
     title: "Balev Corporation 2020",
     images: [
-      "../Balev Corporation 2020/Balev (1).jpg",
-      "../Balev Corporation 2020/Balev (2).jpg",
-      "../Balev Corporation 2020/Balev (3).jpg",
-      "../Balev Corporation 2020/Balev (4).jpg",
-      "../Balev Corporation 2020/Balev (5).jpg"
+      "Balev%20Corporation%202020/Balev%20(1).jpg",
+      "Balev%20Corporation%202020/Balev%20(2).jpg",
+      "Balev%20Corporation%202020/Balev%20(3).jpg",
+      "Balev%20Corporation%202020/Balev%20(4).jpg",
+      "Balev%20Corporation%202020/Balev%20(5).jpg",
+      "Balev%20Corporation%202020/Balev%20(6).jpg"
     ]
   },
   {
     title: "Elite Clinic 2021",
     images: [
-      "../Elite Clinic 2021/Cam01.jpg",
-      "../Elite Clinic 2021/Cam02.jpg",
-      "../Elite Clinic 2021/Cam03.jpg",
-      "../Elite Clinic 2021/Cam04.jpg",
-      "../Elite Clinic 2021/Cam05.jpg"
+      "Elite%20Clinic%202021/Cam01.jpg",
+      "Elite%20Clinic%202021/Cam010.jpg",
+      "Elite%20Clinic%202021/Cam011.jpg",
+      "Elite%20Clinic%202021/Cam012.jpg",
+      "Elite%20Clinic%202021/Cam013.jpg",
+      "Elite%20Clinic%202021/Cam014.jpg",
+      "Elite%20Clinic%202021/Cam015.jpg",
+      "Elite%20Clinic%202021/Cam016.jpg",
+      "Elite%20Clinic%202021/Cam017.jpg",
+      "Elite%20Clinic%202021/Cam02.jpg",
+      "Elite%20Clinic%202021/Cam03.jpg",
+      "Elite%20Clinic%202021/Cam04.jpg",
+      "Elite%20Clinic%202021/Cam05.jpg",
+      "Elite%20Clinic%202021/Cam06.jpg",
+      "Elite%20Clinic%202021/Cam07.jpg",
+      "Elite%20Clinic%202021/Cam08.jpg",
+      "Elite%20Clinic%202021/Cam09.jpg"
     ]
   },
   {
     title: "Gichev sped 2019",
     images: [
-      "../Gichev sped 2019/GichevSped1 (1).jpg",
-      "../Gichev sped 2019/GichevSped1 (2).jpg",
-      "../Gichev sped 2019/GichevSped1 (3).jpg",
-      "../Gichev sped 2019/GichevSped1 (4).jpg",
-      "../Gichev sped 2019/GichevSped1 (5).jpg"
+      "Gichev%20sped%202019/GichevSped1%20(1).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(10).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(11).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(12).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(13).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(2).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(3).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(4).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(5).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(6).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(7).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(8).jpg",
+      "Gichev%20sped%202019/GichevSped1%20(9).jpg"
     ]
   },
   {
     title: "Oliv vilas sv.Vlas 2019",
     images: [
-      "../Oliv vilas sv.Vlas 2019/1O.V..jpg",
-      "../Oliv vilas sv.Vlas 2019/2O.V..jpg",
-      "../Oliv vilas sv.Vlas 2019/3O.V..jpg",
-      "../Oliv vilas sv.Vlas 2019/4O.V..jpg",
-      "../Oliv vilas sv.Vlas 2019/5O.V..jpg"
+      "Oliv%20vilas%20sv.Vlas%202019/1O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/11O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/12O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/1O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/2O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/3O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/4O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/5O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/6O.V..bmp",
+      "Oliv%20vilas%20sv.Vlas%202019/7O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/8O.V..jpg",
+      "Oliv%20vilas%20sv.Vlas%202019/9O.V..jpg"
     ]
   },
   {
@@ -112,11 +205,19 @@ const projects = [
   {
     title: "Work Del Mar 2022",
     images: [
-      "../Work Del Mar 2022/1 (1).jpg",
-      "../Work Del Mar 2022/1 (2).jpg",
-      "../Work Del Mar 2022/1 (3).jpg",
-      "../Work Del Mar 2022/1 (4).jpg",
-      "../Work Del Mar 2022/1 (5).jpg"
+      "Work%20Del%20Mar%202022/1%20(1).jpg",
+      "Work%20Del%20Mar%202022/1%20(10).jpg",
+      "Work%20Del%20Mar%202022/1%20(11).jpg",
+      "Work%20Del%20Mar%202022/1%20(12).jpg",
+      "Work%20Del%20Mar%202022/1%20(13).jpg",
+      "Work%20Del%20Mar%202022/1%20(2).jpg",
+      "Work%20Del%20Mar%202022/1%20(3).jpg",
+      "Work%20Del%20Mar%202022/1%20(4).jpg",
+      "Work%20Del%20Mar%202022/1%20(5).jpg",
+      "Work%20Del%20Mar%202022/1%20(6).jpg",
+      "Work%20Del%20Mar%202022/1%20(7).jpg",
+      "Work%20Del%20Mar%202022/1%20(8).jpg",
+      "Work%20Del%20Mar%202022/1%20(9).jpg"
     ]
   }
 ];
