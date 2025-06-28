7e9650b55ac95eecfafe8f70368936477952a736
V Issa <vissa@192.168.68.101>
2025-06-28 01:28:22 -0400

    Initial commit: Add 20 portfolio website variations

---
+++ projects.js
@@ -0,0 +1,113 @@
+
+const projects = [
+  {
+    title: "Apartament Flavia Garden 2024",
+    images: [
+      "Apartament Flavia Garden 2024/cam01.jpg",
+      "Apartament Flavia Garden 2024/cam02.jpg",
+      "Apartament Flavia Garden 2024/cam03.jpg",
+      "Apartament Flavia Garden 2024/cam04.jpg",
+      "Apartament Flavia Garden 2024/cam05.jpg"
+    ]
+  },
+  {
+    title: "Apartament K55_2021",
+    images: [
+      "Apartament K55_2021/Vladi (1).jpg",
+      "Apartament K55_2021/Vladi (2).jpg",
+      "Apartament K55_2021/Vladi (3).jpg",
+      "Apartament K55_2021/Vladi (4).jpg",
+      "Apartament K55_2021/Vladi (5).jpg"
+    ]
+  },
+  {
+    title: "Apartament Кв. Чайка, Варна_2017",
+    images: [
+      "Apartament Кв. Чайка, Варна_2017/Dnevna01.jpg",
+      "Apartament Кв. Чайка, Варна_2017/Dnevna02.jpg",
+      "Apartament Кв. Чайка, Варна_2017/Dnevna04.jpg",
+      "Apartament Кв. Чайка, Варна_2017/Fotev (2).jpg",
+      "Apartament Кв. Чайка, Варна_2017/Fotev (3).jpg"
+    ]
+  },
+  {
+    title: "Apartament Симфония - Бриз, Варна_ 2019",
+    images: [
+      "Apartament Симфония - Бриз, Варна_ 2019/Alex (1).jpg",
+      "Apartament Симфония - Бриз, Варна_ 2019/Alex (2).jpg",
+      "Apartament Симфония - Бриз, Варна_ 2019/Alex (3).jpg",
+      "Apartament Симфония - Бриз, Варна_ 2019/Alex (4).jpg",
+      "Apartament Симфония - Бриз, Варна_ 2019/Alex (5).jpg"
+    ]
+  },
+  {
+    title: "Apartament Траката, Варна_2021",
+    images: [
+      "Apartament Траката, Варна_2021/1-1-2000x1200.jpg",
+      "Apartament Траката, Варна_2021/10-1-768x512.jpg",
+      "Apartament Траката, Варна_2021/11-1-768x576.jpg",
+      "Apartament Траката, Варна_2021/12-1-768x512.jpg",
+      "Apartament Траката, Варна_2021/13-768x1132.jpg"
+    ]
+  },
+  {
+    title: "Balev Corporation 2020",
+    images: [
+      "Balev Corporation 2020/Balev (1).jpg",
+      "Balev Corporation 2020/Balev (2).jpg",
+      "Balev Corporation 2020/Balev (3).jpg",
+      "Balev Corporation 2020/Balev (4).jpg",
+      "Balev Corporation 2020/Balev (5).jpg"
+    ]
+  },
+  {
+    title: "Elite Clinic 2021",
+    images: [
+      "Elite Clinic 2021/Cam01.jpg",
+      "Elite Clinic 2021/Cam02.jpg",
+      "Elite Clinic 2021/Cam03.jpg",
+      "Elite Clinic 2021/Cam04.jpg",
+      "Elite Clinic 2021/Cam05.jpg"
+    ]
+  },
+  {
+    title: "Gichev sped 2019",
+    images: [
+      "Gichev sped 2019/GichevSped1 (1).jpg",
+      "Gichev sped 2019/GichevSped1 (2).jpg",
+      "Gichev sped 2019/GichevSped1 (3).jpg",
+      "Gichev sped 2019/GichevSped1 (4).jpg",
+      "Gichev sped 2019/GichevSped1 (5).jpg"
+    ]
+  },
+  {
+    title: "Oliv vilas sv.Vlas 2019",
+    images: [
+      "Oliv vilas sv.Vlas 2019/1O.V..jpg",
+      "Oliv vilas sv.Vlas 2019/2O.V..jpg",
+      "Oliv vilas sv.Vlas 2019/3O.V..jpg",
+      "Oliv vilas sv.Vlas 2019/4O.V..jpg",
+      "Oliv vilas sv.Vlas 2019/5O.V..jpg"
+    ]
+  },
+  {
+    title: "Playground Grand Mall Varna 2018",
+    images: [
+      "Playground Grand Mall Varna 2018/Playground (1).jpg",
+      "Playground Grand Mall Varna 2018/Playground (2).jpg",
+      "Playground Grand Mall Varna 2018/Playground (3).jpg",
+      "Playground Grand Mall Varna 2018/Playground (4).jpg",
+      "Playground Grand Mall Varna 2018/Playground (5).jpg"
+    ]
+  },
+  {
+    title: "Work Del Mar 2022",
+    images: [
+      "Work Del Mar 2022/1 (1).jpg",
+      "Work Del Mar 2022/1 (2).jpg",
+      "Work Del Mar 2022/1 (3).jpg",
+      "Work Del Mar 2022/1 (4).jpg",
+      "Work Del Mar 2022/1 (5).jpg"
+    ]
+  }
+];
